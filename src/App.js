78992cbdae72f6bff71d64b4b6ff3b752eebab9a
abhinav-m22 <abhinavpm05@gmail.com>
2023-02-04 16:36:11 +0530

    Added the functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Header from './components/Header';
+import CreateArea from './components/CreateArea';
+import { useState } from 'react';
+import Note from './components/Note';
+import Footer from './components/Footer';
+import './styles.css';
 
 function App() {
+
+  const[notes, setNotes] = useState([]);
+
+  function addNote(newNote){
+    setNotes(prevNotes =>{
+      return[...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <CreateArea 
+        onAdd={addNote}
+      />
+      {notes.map((noteItem, index) =>{
+        return(
+          <Note 
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+          />
+        );
+      })}
+      {/* <Note key={1} title="Note Title" content="Note Content" /> */}
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
+  return <footer>
+    <p>
+      Copyright © {currentYear}
+    </p>
+  </footer>
+}
+
+export default Footer;
