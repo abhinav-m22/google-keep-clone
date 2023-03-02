67c6520555c685f55aeb375b4e5106fd69529547
abhinav-m22 <abhinavpm05@gmail.com>
2023-03-02 16:01:03 +0530

    Added the backend configuration - motoko

--- src/declarations/dkeep/dkeep.did.d.ts
+++ src/declarations/dkeep/dkeep.did.d.ts
@@ -1,2 +1,7 @@
 import type { Principal } from '@dfinity/principal';
-export interface _SERVICE { 'greet' : (arg_0: string) => Promise<string> }
+export interface Note { 'title' : string, 'content' : string }
+export interface _SERVICE {
+  'createNote' : (arg_0: string, arg_1: string) => Promise<undefined>,
+  'readNotes' : () => Promise<Array<Note>>,
+  'removeNote' : (arg_0: bigint) => Promise<undefined>,
+}
--- src/dkeep_assets/src/components/App.jsx
+++ src/dkeep_assets/src/components/App.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { dkeep } from "../../../declarations/dkeep";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
+      dkeep.createNote(newNote.title, newNote.content)
       return [...prevNotes, newNote];
     });
   }
 
   function deleteNote(id) {
+
+    dkeep.removeNote(id);
+
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
     });
+  }
+
+  useEffect(() => {
+    // console.log("useEffect");
+    fetchData();
+  }, []);
+
+  async function fetchData(){
+    const notesArray = await dkeep.readNotes();
+    setNotes(notesArray);
   }
 
   return (
